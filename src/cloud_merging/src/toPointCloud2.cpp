d1a66a940f6ca3ded4a3f8e968f843e32dd017b7
dearmoon <279892049@qq.com>
2024-04-29 15:23:10 +0800

    增加点云融合

---
+++ src/cloud_merging/src/toPointCloud2.cpp
@@ -0,0 +1,15 @@
+#include <ros/ros.h>
+#include <rosbag/bag.h>
+#include <rosbag/view.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <sensor_msgs/PointCloud.h>
+#include <rosbag_tools/CustomMsg.h>
+#include <message_filters/sync_policies/approximate_time.h>
+#include <message_filters/subscriber.h>
+
+
+/*
+ *  将订阅的点云数据转换为PointCloud2格式
+ *  原点云数据格式：CustomMsg和PointCloud
+ *  转换后数据格式：PointCloud2
+*/
